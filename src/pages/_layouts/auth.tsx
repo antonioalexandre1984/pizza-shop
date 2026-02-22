dc82cb6f7c828353108bff56f1390a3c653ccbd4
antonioalexandre1984 <antonioalexandre1984@gmail.com>
2026-02-22 11:45:39 -0300

    finalizando estilização da pagina de login

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -1,10 +1,29 @@
+import { Pizza } from 'lucide-react'
 import { Outlet } from 'react-router-dom'
 
 export const AuthLayout = () => {
   return (
-    <div>
-      <h1>Auth Layout</h1>
-      <Outlet />
+    <div className="grid min-h-screen grid-cols-2">
+      <div className="border-foreground/5 bg-muted text-muted-foreground flex h-full flex-col justify-between border-r p-10">
+        <div className="text-foreground flex items-center gap-3 text-lg font-medium">
+          <Pizza className="h-5 w-5" size={28} />
+          <span className="font-semibold">pizza.shop</span>
+        </div>
+        <div className="space-y-6">
+          <h1 className="text-3xl font-bold tracking-tight">
+            Bem-vindo de volta
+          </h1>
+          <p className="text-muted-foreground text-sm">
+            Insira suas credenciais para acessar o painel do parceiro.
+          </p>
+        </div>
+        <footer className="text-sm">
+          Painel do Parceiro &copy; pizza.shop - {new Date().getFullYear()}
+        </footer>
+      </div>
+      <div className="flex flex-col items-center justify-center p-10">
+        <Outlet />
+      </div>
     </div>
   )
 }
--- src/pages/auth/signIn.tsx
+++ src/pages/auth/signIn.tsx
@@ -1,10 +1,64 @@
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
 import { Helmet } from 'react-helmet-async'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
+
+const signInForm = z.object({
+  email: z.string().email('Email inválido').min(1, 'Informe seu email'),
+})
+
+type SignInFormData = z.infer<typeof signInForm>
 
 export const SignIn = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SignInFormData>({
+    resolver: zodResolver(signInForm),
+  })
+
+  async function handleSignIn(data: SignInFormData) {
+    console.log(data)
+    await new Promise((resolve) =>
+      setTimeout(() => {
+        resolve(true)
+      }, 2000),
+    )
+  }
+
   return (
     <>
       <Helmet title="Sign In" />
-      <div>signIn</div>
+      <div className="p-8">
+        <div className="flex w-87.5 flex-col justify-center gap-6">
+          <div className="flex flex-col gap-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Acessar Painel
+            </h1>
+            <p className="text-muted-foreground text-sm">
+              Acompanhe suas vendas pelo painel do parceiro.
+            </p>
+          </div>
+          <form className="space-y-4" onSubmit={handleSubmit(handleSignIn)}>
+            <div className="space-y-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="Seu email"
+                {...register('email')}
+              />
+            </div>
+            <Button disabled={isSubmitting} type="submit" className="w-full">
+              Entrar
+            </Button>
+          </form>
+        </div>
+      </div>
     </>
   )
 }
